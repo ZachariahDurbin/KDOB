6c427498cec490292bf0c238810fa812843ffbc8
Zachariah Durbin <zachariahdurbin@gmail.com>
2021-10-15 19:50:22 -0500

    Add GraphQL Boilerplate

---
+++ backend/src/index.js
@@ -0,0 +1,25 @@
+const { ApolloServer, gql } = require('apollo-server');
+
+
+const books = [
+  {
+    title: 'The Awakening',
+    author: 'Kate Chopin',
+  },
+  {
+    title: 'City of Glass',
+    author: 'Paul Auster',
+  },
+];
+const typeDefs = gql;
+
+const resolvers = {
+  Query: {
+    books: () => books,
+  },
+};
+const server = new ApolloServer({ typeDefs, resolvers });
+
+server.listen().then(({ url }) => {
+  console.log(`🚀  Server ready at ${url}`);
+});

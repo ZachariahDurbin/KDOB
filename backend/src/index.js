c6e8350964f6220a4c525082557cf902c5f4e394
Zachariah Durbin <zachariahdurbin@gmail.com>
2021-10-21 10:08:50 -0500

    Create Sign Up and Sign In Functions

--- backend/src/index.js
+++ backend/src/index.js
@@ -3,6 +3,7 @@ const express = require('express');
 const { graphqlHTTP } = require('express-graphql');
 const { buildSchema } = require('graphql');
 const { MongoClient } = require('mongodb');
+const bcrypt = require('bcryptjs');
 //Setting up Environment Variables...
 const dotenv = require('dotenv');
 dotenv.config();
@@ -18,42 +19,83 @@ const database = client.db(DB_NAME);
 //Schemas used for Queries
 var schema = buildSchema(`
   type Query {
-    hello: String
-    kek: Float
   }
 
   type Mutation {
-    createUser(name: String!): String
+    signUp(input: SignUpInput): AuthUser!
+    signIn(input: SignInInput): AuthUser!
+  }
+  
+  input SignUpInput{
+    email: String!,
+    password: String!,
+    name: String! 
+  }
+
+  input SignInInput{
+    email: String!,
+    password: String!
+  }
+
+  type AuthUser {
+    user: User!
+    token: String!
+  }
+
+  type User {
+    id: ID!
+    name: String!
+    email: String!
   }
 `);
 
 
 // The root provides a resolver function for each API endpoint
 var root = {
-  hello: () => {
-    return 'Hello world!';
-  },
-  kek: () => {
-    return 3.3;
-  },
-  createUser: async({ name }) =>{
+
+  //Sign Up Function
+  signUp: async ({ input }) =>{
+    const hashedPassword = bcrypt.hashSync(input.password); //Hash Password
+    console.log("Do they match? " + bcrypt.compareSync(input.password, hashedPassword)); //Proving hash is correct
     const newUser = {
-      ...name,
-      createdAt: new Date().toISOString(),
+      ...input,
+      password: hashedPassword
     }
-    const result = await database.collection('Users').insertOne(newUser);
-    console.log("Added!");
-    return "Added!";
+    const result = await database.collection('Users').insertOne(newUser); //Inputs newUser into Database
+
+    if(!result.acknowledged)throw new Error('Something went wrong with creation'); //If a problem occurs, throw error
+    
+    //If the process works, return new user to client
+    return {
+      user: {
+        ...newUser,
+        id : newUser._id
+      },
+      token: 'token'
+    }
+    
+  },
+
+  //Sign In Function
+  signIn: async ({ input }) => {
+    const foundUser = await database.collection('Users').findOne({ email: input.email}); //Attempts to pull credentials from database via email...
+    if(!foundUser) throw new Error('Invalid credentials'); //If no user is found, throw error saying "Invalid credentials"
+    const isPasswordCorrect = bcrypt.compareSync(input.password, foundUser.password); //Proving hash is correct
+    if(!isPasswordCorrect) throw new Error('Invalid credentials'); //If password is wrong, throw error saying "Invalid credentials"
+    return {
+      user:{
+        ...foundUser,
+        id : foundUser._id
+      },
+      token: 'token'
+    }
   }
 };
 
 
 //Async Start Function
 const start = async () => {
-  
-
-  //Starting Express...
-  var app = express();
+  var app = express(); //Starting Express...
 
   //Using Home Directory for GraphQL Basic Testing Center...
   app.use('/', graphqlHTTP({
@@ -61,7 +103,6 @@ const start = async () => {
     rootValue: root,
     graphiql: true,
   }));
-
 
   //Listening to Port 4000, and Giving User Link to Connect...
   app.listen(4000);

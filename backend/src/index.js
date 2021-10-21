628bb376448d05d334ee901edfccb1416ca07ad8
Zachariah Durbin <zachariahdurbin@gmail.com>
2021-10-20 20:24:29 -0500

    Add basic manipulation of database

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,26 +1,78 @@
-var express = require('express');
-var { graphqlHTTP } = require('express-graphql');
-var { buildSchema } = require('graphql');
+//Creating all Constant Variables
+const express = require('express');
+const { graphqlHTTP } = require('express-graphql');
+const { buildSchema } = require('graphql');
+const dotenv = require('dotenv');
+const { MongoClient } = require('mongodb');
+//Setting up Environment Variables...
+dotenv.config();
+const { DB_URI, DB_NAME } = process.env;
 
-// Construct a schema, using GraphQL schema language
+
+
+//Schemas used for Queries
 var schema = buildSchema(`
   type Query {
     hello: String
   }
+
+  type Mutation {
+    createUser(name: String!): String
+  }
 `);
+
+
 
 // The root provides a resolver function for each API endpoint
 var root = {
   hello: () => {
     return 'Hello world!';
   },
+  createUser: async({ name }, { context }) =>{
+    const newUser = {
+      ...name,
+      createdAt: new Date().toISOString(),
+    }
+    console.log(name);
+    console.log(context);
+    const client = new MongoClient(DB_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+    await client.connect();
+    const result = await client.db(DB_NAME).collection('Users').insert(newUser);
+    client.close();
+    console.log(result);
+
+    return "Added!";
+  }
 };
 
-var app = express();
-app.use('/graphql', graphqlHTTP({
-  schema: schema,
-  rootValue: root,
-  graphiql: true,
-}));
-app.listen(4000);
-console.log('Running a GraphQL API server at http://localhost:4000/graphql');
+
+
+//Async Start Function
+const start = async () => {
+
+  //Connecting to the MongoDB Database...
+  const client = new MongoClient(DB_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+  await client.connect();
+  const db = client.db(DB_NAME);
+  
+
+  //Starting Express...
+  var app = express();
+
+  //Using Home Directory for GraphQL Basic Testing Center...
+  app.use('/', graphqlHTTP({
+    schema: schema,
+    rootValue: root,
+    graphiql: true,
+  }));
+
+
+  //Listening to Port 400, and Giving User Link to Connect...
+  app.listen(4000);
+  console.log('🚀 KDOB is at least running, right? Let\'s make sure it\'s actually working: http://localhost:4000/');
+}
+
+
+
+//Let's Go!
+start();

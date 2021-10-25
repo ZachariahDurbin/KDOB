42760eee3e783b0f675762dc3c2777c41167f08a
Zachariah <zachariahdurbin@gmail.com>
2021-10-25 15:35:07 -0500

    Create business logic area and put board logic there

--- backend/src/graphql/board.js
+++ backend/src/graphql/board.js
@@ -1,10 +1,10 @@
 const graphql = require('graphql');
-const { buildResolveInfo } = require('graphql/execution/execute');
 const bcrypt = require('bcryptjs');
-const { GraphQLObjectType, GraphQLString, GraphQLSchema, GraphQLID, GraphQLInt } = graphql;
-const Board = require('../mongo/board');
+const { GraphQLObjectType, GraphQLString, GraphQLID, GraphQLInt } = graphql;
+const MongoBoard = require('../mongo/board');
+const LogicBoard = require('../businesslogic/board');
 
-
+//Players for each Game
 const PlayerType = new GraphQLObjectType({
     name: 'Players',
     fields: () => ({
@@ -13,13 +13,15 @@ const PlayerType = new GraphQLObjectType({
     })
 });
 
+//Unique positions only a single player can access (endzones and bars)
 const PlayerSpecificPositionType = new GraphQLObjectType({
     name: 'PlayerSpecificPositions',
     fields: () => ({
         amount: { type: GraphQLInt }
     })
-})
+});
 
+//Common positions
 const PositionType = new GraphQLObjectType({
     name: 'Positions',
     fields: () => ({
@@ -28,200 +30,145 @@ const PositionType = new GraphQLObjectType({
     })
 });
 
+//All board positions
 const BoardType = new GraphQLObjectType({
     name: 'Board',
     fields: () => ({
         position1: {
             type: PositionType,
-            resolve(parent, args){
-                return parent.position1
-            }
+            resolve(parent, args){ return parent.position1 }
         },
         position2: {
             type: PositionType,
-            resolve(parent, args){
-                return parent.position2
-            }
+            resolve(parent, args){ return parent.position2 }
         },
         position3: {
             type: PositionType,
-            resolve(parent, args){
-                return parent.position3
-            }
+            resolve(parent, args){ return parent.position3 }
         },
         position4: {
             type: PositionType,
-            resolve(parent, args){
-                return parent.position4
-            }
+            resolve(parent, args){ return parent.position4 }
         },
         position5: {
             type: PositionType,
-            resolve(parent, args){
-                return parent.position5
-            }
+            resolve(parent, args){ return parent.position5 }
         },
         position6: {
             type: PositionType,
-            resolve(parent, args){
-                return parent.position6
-            }
+            resolve(parent, args){ return parent.position6 }
         },
         position7: {
             type: PositionType,
-            resolve(parent, args){
-                return parent.position7
-            }
+            resolve(parent, args){ return parent.position7 }
         },
         position8: {
             type: PositionType,
-            resolve(parent, args){
-                return parent.position8
-            }
+            resolve(parent, args){ return parent.position8 }
         },
         position9: {
             type: PositionType,
-            resolve(parent, args){
-                return parent.position9
-            }
+            resolve(parent, args){ return parent.position9 }
         },
         position10: {
             type: PositionType,
-            resolve(parent, args){
-                return parent.position10
-            }
+            resolve(parent, args){ return parent.position10 }
         },
         position11: {
             type: PositionType,
-            resolve(parent, args){
-                return parent.position11
-            }
+            resolve(parent, args){ return parent.position11 }
         },
         position12: {
             type: PositionType,
-            resolve(parent, args){
-                return parent.position12
-            }
+            resolve(parent, args){ return parent.position12 }
         },
         position13: {
             type: PositionType,
-            resolve(parent, args){
-                return parent.position13
-            }
+            resolve(parent, args){ return parent.position13 }
         },
         position14: {
             type: PositionType,
-            resolve(parent, args){
-                return parent.position14
-            }
+            resolve(parent, args){ return parent.position14 }
         },
         position15: {
             type: PositionType,
-            resolve(parent, args){
-                return parent.position15
-            }
+            resolve(parent, args){ return parent.position15 }
         },
         position16: {
             type: PositionType,
-            resolve(parent, args){
-                return parent.position16
-            }
+            resolve(parent, args){ return parent.position16 }
         },
         position17: {
             type: PositionType,
-            resolve(parent, args){
-                return parent.position17
-            }
+            resolve(parent, args){ return parent.position17 }
         },
         position18: {
             type: PositionType,
-            resolve(parent, args){
-                return parent.position18
-            }
+            resolve(parent, args){ return parent.position18 }
         },
         position19: {
             type: PositionType,
-            resolve(parent, args){
-                return parent.position19
-            }
+            resolve(parent, args){ return parent.position19 }
         },
         position20: {
             type: PositionType,
-            resolve(parent, args){
-                return parent.position20
-            }
+            resolve(parent, args){ return parent.position20 }
         },
         position21: {
             type: PositionType,
-            resolve(parent, args){
-                return parent.position21
-            }
+            resolve(parent, args){ return parent.position21 }
         },
         position22: {
             type: PositionType,
-            resolve(parent, args){
-                return parent.position22
-            }
+            resolve(parent, args){ return parent.position22 }
         },
         position23: {
             type: PositionType,
-            resolve(parent, args){
-                return parent.position23
-            }
+            resolve(parent, args){ return parent.position23 }
         },
         position24: {
             type: PositionType,
-            resolve(parent, args){
-                return parent.position24
-            }
+            resolve(parent, args){ return parent.position24 }
         },
         whitebar: {
             type: PlayerSpecificPositionType,
-            resolve(parent, args){
-                return parent.whitebar
-            }
+            resolve(parent, args){ return parent.whitebar }
         }, 
         blackbar: {
             type: PlayerSpecificPositionType,
-            resolve(parent, args){
-                return parent.blackbar
-            }
+            resolve(parent, args){ return parent.blackbar }
         }, 
         whiteremoval: {
             type: PlayerSpecificPositionType,
-            resolve(parent, args){
-                return parent.whiteremoval
-            }
+            resolve(parent, args){ return parent.whiteremoval }
         },
         blackremoval: {
             type: PlayerSpecificPositionType,
-            resolve(parent, args){
-                return parent.blackremoval
-            }
+            resolve(parent, args){ return parent.blackremoval }
         },
     })
 });
 
+//Game Information
 const GameType = new GraphQLObjectType({
     name: 'Game',
     fields: () => ({
         id: { type: GraphQLID },
         players: { 
             type: PlayerType,
-            resolve(parent, args){
-                return parent.players;
-            }
+            resolve(parent, args){ return parent.players }
         },
         board: {
             type: BoardType,
-            resolve(parent, args){
-                return parent.positions
-            }
+            resolve(parent, args){ return parent.positions }
         }
     })
 });
 
+//Queries exported
 const BoardQueries = {
+
+    //Ask for board with id
     getBoard:{
         type: GameType,
         args: { 
@@ -231,6 +178,7 @@ const BoardQueries = {
             return Board.findById(args.id);
         }
     },
+    //Ask for all boards with nothing
     getAll:{
         type: GameType,
         resolve(parent, args){
@@ -239,130 +187,17 @@ const BoardQueries = {
     }
 }
 
+//Mutations exported
 const BoardMutations = {
+    //Create new blank board with player names
     newBoard: {
         type: GameType,
         args: {
-            player1: {type: GraphQLString},
+            player1: {type: GraphQLString },
             player2: { type: GraphQLString }
         },
         resolve(parent, args){
-            let board = new Board({
-                players: { 
-                    player1: args.player1,
-                    player2: args.player2
-                },
-                positions: {
-                    position1: {
-                        color: "white",
-                        amount: 2
-                    },
-                    position2: {
-                        color: null,
-                        amount: 0
-                    },
-                    position3: {
-                        color: null,
-                        amount: 0
-                    },
-                    position4: {
-                        color: null,
-                        amount: 0
-                    },
-                    position5: {
-                        color: null,
-                        amount: 0
-                    },
-                    position6: {
-                        color: "black",
-                        amount: 5
-                    },
-                    position7: {
-                        color: "whte",
-                        amount: 0
-                    },
-                    position8: {
-                        color: "black",
-                        amount: 3
-                    },
-                    position9: {
-                        color: null,
-                        amount: 0
-                    },
-                    position10: {
-                        color: null,
-                        amount: 0
-                    },
-                    position11: {
-                        color: null,
-                        amount: 0
-                    },
-                    position12: {
-                        color: "white",
-                        amount: 5
-                    },
-                    position13: {
-                        color: "black",
-                        amount: 5
-                    },
-                    position14: {
-                        color: null,
-                        amount: 0
-                    },
-                    position15: {
-                        color: null,
-                        amount: 0
-                    },
-                    position16: {
-                        color: null,
-                        amount: 0
-                    },
-                    position17: {
-                        color: "white",
-                        amount: 3
-                    },
-                    position18: {
-                        color: null,
-                        amount: 0
-                    },
-                    position19: {
-                        color: "white",
-                        amount: 5
-                    },
-                    position20: {
-                        color: null,
-                        amount: 0
-                    },
-                    position21: {
-                        color: null,
-                        amount: 0
-                    },
-                    position22: {
-                        color: null,
-                        amount: 0
-                    },
-                    position23: {
-                        color: null,
-                        amount: 0
-                    },
-                    position24: {
-                        color: "black",
-                        amount: 2
-                    },
-                    whitebar: {
-                        amount: 0
-                    },
-                    blackbar: {
-                        amount: 0
-                    },
-                    whiteremoval: {
-                        amount: 0
-                    },
-                    blackremoval: {
-                        amount: 0
-                    }
-                }
-            })
+            let board = new MongoBoard(LogicBoard.createNewBoard(args));
             return board.save();
         }
     },
@@ -374,14 +209,14 @@ const BoardMutations = {
             newPosition: { type: GraphQLString }
         },
         resolve(parent, args){
-            let board = Board.findById(args.id);
+            let board = MongoBoard.findById(args.id);
             console.log(args);
             //let board = new Board({
             //    players: { 
             //        player1: args.player1,
             //        player2: args.player2
             //    },
-            return Board.findById(args.id);
+            return MongoBoard.findById(args.id);
         }
     }
 }
